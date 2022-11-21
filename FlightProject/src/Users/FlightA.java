56628b47194993625c4a6a1cd06979f5a42dba76
abriellek <abriellek@localhost>
2022-11-21 18:34:40 -0500

    ohboyyy

---
+++ FlightProject/src/Users/FlightA.java
@@ -0,0 +1,22 @@
+package Users;
+
+import java.util.*;
+import java.time.*;
+import java.math.BigDecimal;
+
+public class FlightA {
+	private LocalDate flightDate;
+	private LocalTime departureTime;
+	private LocalTime arrivalTime;
+	private int flightNumber;
+	private String airline;
+	private String departingCity;
+	private String arrivalCity;
+	private BigDecimal cost;
+	private int mainCabinSeats;
+	private int firstClassSeats;
+	
+	
+	
+
+}
--- FlightProject/src/Users/User.java
+++ FlightProject/src/Users/User.java
@@ -1,5 +1,100 @@
 package Users;
 
-public class User {
+import java.util.*;
 
+public class User {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String zipcode;
+	private String state;
+	private String username;
+	private String password;
+	private String email;
+	private String ssn;
+	private String securityQuestion;
+	private String securityAnswer;
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+	
+	//Login to user account
+	public boolean login(String username, String password) {
+		if (this.username == username && this.password == password) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean logout() {
+		return true;
+	}
+	
+	
 }
