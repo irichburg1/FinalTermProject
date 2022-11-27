82037e3f09accf17c4528e832e08e736cea25aba
isaiahrichburg <isaiahrichburg@localhost>
2022-11-26 20:00:50 -0500

    Menu Temp Attributes

---
+++ FlightProject/src/classes/Menu.java
@@ -0,0 +1,27 @@
+package classes;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Menu {
+	
+	private String tempFirstName ;
+	private String tempLastName;
+	private String tempAddress;
+	private String tempZipcode;
+	private String tempState;
+	private String tempUsername;
+	private String tempPassword;
+	private String tempEmail;
+	private String tempSsn;
+	private String tempSecurityQuestion;
+	private String tempSecurityAnswer;
+
+	public static void register1 () {
+			
+		}
+		
+	}
+	
+	
+
